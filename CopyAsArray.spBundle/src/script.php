98cca416fdb987bc8293e08e441d05814671cf62
Nhat Tran <thmnhat@gmail.com>
2014-09-30 11:37:03 +0700

    Add CopyAsArray bundle

---
+++ CopyAsArray.spBundle/src/script.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+
+function copyToClipboard($content)
+{
+    $cmd = 'echo '.  escapeshellarg($content) . ' | __CF_USER_TEXT_ENCODING=' . posix_getuid() . ':0x8000100:0x8000100 pbcopy';
+    shell_exec($cmd);
+}
+
+$stdIn = fopen('php://stdin', 'r');
+$result = array();
+
+while ($row = fgetcsv($stdIn, 0)) {
+    $result[] = $row;
+}
+
+$columns = array_shift($result);
+
+foreach ($result as $key => $data) {
+    $result[$key] = array_combine($columns, $data);
+}
+
+$count = count($result);
+
+if ($count) {
+    if ($count == 1) {
+        $result = reset($result);
+    }
+
+    copyToClipboard(var_export($result, true));
+}
